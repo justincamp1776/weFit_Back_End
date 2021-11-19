437a5bc18e59227e27106e76aa4c91a4193049b5
justincamp1776 <justincamp1776@gmail.com>
2021-11-19 11:06:04 -0500

    created and tested user patch request

--- drf_jwt_capstone_backend/authentication/views.py
+++ drf_jwt_capstone_backend/authentication/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from .serializers import RegistrationSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView;
-from rest_framework import generics
+from rest_framework import generics, status
 from django.http.response import Http404
 from .models import User
 from rest_framework.permissions import AllowAny
@@ -28,3 +28,11 @@ class UserDetail(APIView):
         user = self.get_object(pk)
         serializer = RegistrationSerializer(user)
         return Response(serializer.data)
+
+    def patch(self, request, pk):
+        user=self.get_object(pk)
+        serializer=RegistrationSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
