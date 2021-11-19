707c092b1341f4015b90f29fcc7640036d904f6e
justincamp1776 <justincamp1776@gmail.com>
2021-11-19 09:38:47 -0500

    Added detail view in User Views to retrieve all User properties

--- drf_jwt_capstone_backend/authentication/urls.py
+++ drf_jwt_capstone_backend/authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView
+from .views import RegisterView, UserDetail
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('<int:pk>/', UserDetail.as_view())
 ]
--- drf_jwt_capstone_backend/authentication/views.py
+++ drf_jwt_capstone_backend/authentication/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 from .serializers import RegistrationSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView;
 from rest_framework import generics
+from django.http.response import Http404
+from .models import User
 from rest_framework.permissions import AllowAny
 User = get_user_model()
 
@@ -10,3 +14,17 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
 
+class UserDetail(APIView):
+    
+    # permission_classes = (AllowAny)
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        serializer = RegistrationSerializer(user)
+        return Response(serializer.data)
